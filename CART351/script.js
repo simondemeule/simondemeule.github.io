e51986b08fe68ab9e8f8228bf505cb33c963f72e
simondemeule <simondemeule@gmail.com>
2018-09-25 12:31:01 -0400

    Better fix for Safari history glitch

--- CART351/script.js
+++ CART351/script.js
@@ -14,7 +14,10 @@ $(".top-contact-button").click(function (event) {
 $(".top-portfolio-button").click(function(event) {
     $(".obstructor").css("top", "0");
     setTimeout(function() {
-        $(".obstructor").css("top", "150vh"); // necessary to prevent glitch on safari history changes
         window.location.href = "/"
     }, 1000);
 })
+
+window.onpageshow = function(event) {
+    $(".obstructor").css("top", "100vh");
+}
